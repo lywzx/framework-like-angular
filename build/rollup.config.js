3be1aed02b6cb350f40e2920a05e20c4cd3afff2
lywzx <liuyang@lyblog.net>
2020-07-30 21:56:43 +0800

    feat(add build): add ts build

---
+++ build/rollup.config.js
@@ -0,0 +1,34 @@
+import { createEntries } from './rollup-base.config';
+
+const config = [
+  {
+    input: 'src/index.ts',
+    file: 'lib/index.esm.browser.js',
+    format: 'es',
+    browser: true,
+    transpile: false,
+    env: 'development',
+  },
+  {
+    input: 'src/index.ts',
+    file: 'lib/index.esm.browser.min.js',
+    format: 'es',
+    browser: true,
+    transpile: false,
+    minify: true,
+    env: 'production',
+  },
+  // todo remove transpile
+  { input: 'src/index.ts', file: 'lib/index.esm.js', format: 'es', transpile: false, env: 'development' },
+  { input: 'src/index.ts', file: 'lib/index.js', format: 'umd', transpile: false, env: 'development' },
+  { input: 'src/index.ts', file: 'lib/index.min.js', format: 'umd', transpile: false, minify: true, env: 'production' },
+  { input: 'src/index.ts', file: 'lib/index.common.js', format: 'cjs', transpile: false, env: 'development' },
+];
+const dirs = [
+  'core',
+  'react-adapter',
+  'redux',
+  'testing',
+  'vue-adapter'
+];
+export default dirs.map(dir => createEntries(config, dir, '')).flat();
